570f1a57bd2e1c8f6bbbf70a3a04927a07f4db66
Aaron Kim <ajkim19@gmail.com>
2025-01-17 03:42:05 +0000

    Added DOM variables for changePhotoPreview function

---
+++ ts/main.ts
@@ -0,0 +1,4 @@
+const photoURL = document.querySelector<HTMLInputElement>(".photo-url");
+if (!photoURL) throw new Error("photoURL does not exist");
+const photoPreview = document.querySelector<HTMLImageElement>(".photo-preview");
+if (!photoPreview) throw new Error("photoPreview does not exist");
