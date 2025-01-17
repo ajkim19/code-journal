452f34cbc6e6957a1cedea4dceb9492e313ac17f
Aaron Kim <ajkim19@gmail.com>
2025-01-17 06:20:37 +0000

    Submitting a form now stores the form's input values in a new object and assigns an entryId property to the new object taken from the nextEntryId property of the data model and increments the nextEntryId property of the data model so that if another entry is submitted later, it will receive a different entryId

--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,20 @@
 interface Entry {
-  title: String;
-  photoURL
+  title: string;
+  photoURL: string;
+  notes?: string;
+  entryId: number;
 }
 
 const $photoURL = document.querySelector<HTMLInputElement>("#photo-url");
 if (!$photoURL) throw new Error("$photoURL does not exist");
 const $photoPreview = document.querySelector<HTMLImageElement>(".photo-preview");
 if (!$photoPreview) throw new Error("$photoPreview does not exist");
-const $entryForm = document.querySelector('form.entry-form');
+const $entryForm = document.querySelector<HTMLInputElement>('form.entry-form');
 if (!$entryForm) throw new Error('$entryForm does not exist');
+const $title = document.querySelector<HTMLInputElement>('.title');
+if (!$title) throw new Error('$title does not exist');
+const $notes = document.querySelector<HTMLTextAreaElement>('.notes');
+if (!$notes) throw new Error('$notes does not exist');
 
 function changePhotoPreview(): void {
   if($photoURL && $photoPreview) {
@@ -17,9 +23,17 @@ function changePhotoPreview(): void {
 }
 
 function submitForm(event: Event): void {
-  if ($entryForm) {
+  if ($entryForm && $title && $photoURL && $notes) {
     event.preventDefault();
     console.log('Form submitted');
+    const entry: Entry = {
+      title: $title.value,
+      photoURL: $photoURL.value,
+      notes: $notes.value,
+      entryId: data.nextEntryId
+    }
+    data.entries.push(entry);
+    data.nextEntryId++
   }
 }
 
