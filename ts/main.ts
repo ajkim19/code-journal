f7fbf1f5197bcf09093b24213bbcf92f49481595
Aaron Kim <ajkim19@gmail.com>
2025-01-17 03:50:34 +0000

    Completed changePhotoPreview function #1

--- ts/main.ts
+++ ts/main.ts
@@ -2,3 +2,9 @@ const photoURL = document.querySelector<HTMLInputElement>(".photo-url");
 if (!photoURL) throw new Error("photoURL does not exist");
 const photoPreview = document.querySelector<HTMLImageElement>(".photo-preview");
 if (!photoPreview) throw new Error("photoPreview does not exist");
+
+function changePhotoPreview(): void {
+  if(photoURL && photoPreview) {
+    photoPreview.src = photoURL.textContent;
+  }
+}
