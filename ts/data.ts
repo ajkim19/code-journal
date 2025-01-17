487f3c2456e1fd63a042f2ac91a0892f0ace1fe6
Aaron Kim <ajkim19@gmail.com>
2025-01-17 07:06:39 +0000

    Added function that reads the JSON from localStorage returned the parsed value (or a default data model if it does not exist #1

--- ts/data.ts
+++ ts/data.ts
@@ -4,3 +4,8 @@ const data = {
   editing: null,
   nextEntryId: 1,
 };
+
+function writeData(): void {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-storage', dataJSON);
+}
