76c21df5a972a1fe44283d14477ce91fdc16c943
Aaron Kim <ajkim19@gmail.com>
2025-01-17 06:02:20 +0000

    Added a 'submit' event listener to the form element that prevents the page from refreshing when the form is submitted

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-var photoURL = document.querySelector("#photo-url");
-if (!photoURL)
-    throw new Error("photoURL does not exist");
-var photoPreview = document.querySelector(".photo-preview");
-if (!photoPreview)
-    throw new Error("photoPreview does not exist");
+var $photoURL = document.querySelector("#photo-url");
+if (!$photoURL)
+    throw new Error("$photoURL does not exist");
+var $photoPreview = document.querySelector(".photo-preview");
+if (!$photoPreview)
+    throw new Error("$photoPreview does not exist");
+var $entryForm = document.querySelector('form.entry-form');
+if (!$entryForm)
+    throw new Error('$entryForm does not exist');
 function changePhotoPreview() {
-    if (photoURL && photoPreview) {
-        photoPreview.setAttribute('src', photoURL.value);
+    if ($photoURL && $photoPreview) {
+        $photoPreview.setAttribute('src', $photoURL.value);
     }
 }
-photoURL.addEventListener('keydown' || 'paste', changePhotoPreview);
+function submitForm(event) {
+    if ($entryForm) {
+        event.preventDefault();
+        console.log('Form submitted');
+    }
+}
+$photoURL.addEventListener('keydown' || 'paste', changePhotoPreview);
+$entryForm.addEventListener('submit', submitForm);
--- ts/main.ts
+++ ts/main.ts
@@ -1,12 +1,27 @@
-const photoURL = document.querySelector<HTMLInputElement>("#photo-url");
-if (!photoURL) throw new Error("photoURL does not exist");
-const photoPreview = document.querySelector<HTMLImageElement>(".photo-preview");
-if (!photoPreview) throw new Error("photoPreview does not exist");
+interface Entry {
+  title: String;
+  photoURL
+}
+
+const $photoURL = document.querySelector<HTMLInputElement>("#photo-url");
+if (!$photoURL) throw new Error("$photoURL does not exist");
+const $photoPreview = document.querySelector<HTMLImageElement>(".photo-preview");
+if (!$photoPreview) throw new Error("$photoPreview does not exist");
+const $entryForm = document.querySelector('form.entry-form');
+if (!$entryForm) throw new Error('$entryForm does not exist');
 
 function changePhotoPreview(): void {
-  if(photoURL && photoPreview) {
-    photoPreview.setAttribute('src', photoURL.value);
+  if($photoURL && $photoPreview) {
+    $photoPreview.setAttribute('src', $photoURL.value);
   }
 }
 
-photoURL.addEventListener('keydown' || 'paste', changePhotoPreview);
+function submitForm(event: Event): void {
+  if ($entryForm) {
+    event.preventDefault();
+    console.log('Form submitted');
+  }
+}
+
+$photoURL.addEventListener('keydown' || 'paste', changePhotoPreview);
+$entryForm.addEventListener('submit', submitForm);
